b41fe3362bda81c4c3c699f53e129b347300c50d
juniortopled <topledaracaju@gmail.com>
2025-01-20 12:05:27 -0300

    Update _inquiry.sh

--- lib/_inquiry.sh
+++ lib/_inquiry.sh
@@ -11,7 +11,7 @@ get_mysql_root_password() {
 get_link_git() {
   
   print_banner
-  printf "${WHITE} 💻 Insira o link do GITHUB do Atendechat que deseja instalar:${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Insira o link do GITHUB Paipa que deseja instalar:${GRAY_LIGHT}"
   printf "\n\n"
   read -p "> " link_git
 }
@@ -201,14 +201,14 @@ software_dominio() {
 inquiry_options() {
   
   print_banner
-  printf "${WHITE} 💻 Bem vindo(a) ao Gerenciador Atendechat, Selecione abaixo a proxima ação!${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Bem vindo(a) ao Gerenciador Paipa, Selecione abaixo a proxima ação!${GRAY_LIGHT}"
   printf "\n\n"
-  printf "   [0] Instalar Atendechat\n"
-  printf "   [1] Atualizar Atendechat\n"
-  printf "   [2] Deletar Atendechat\n"
-  printf "   [3] Bloquear Atendechat\n"
-  printf "   [4] Desbloquear Atendechat\n"
-  printf "   [5] Alter. dominio Atendechat\n"
+  printf "   [0] Instalar Paipa\n"
+  printf "   [1] Atualizar Paipa\n"
+  printf "   [2] Deletar Paipa\n"
+  printf "   [3] Bloquear Paipa\n"
+  printf "   [4] Desbloquear Paipa\n"
+  printf "   [5] Alter. dominio Paipa\n"
   printf "\n"
   read -p "> " option
 
